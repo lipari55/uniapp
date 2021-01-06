0ec4a9a92871c24fe01d49ac59a1fc8b445615ec
lipari <66412384+lipari55@users.noreply.github.com>
2021-01-06 19:04:27 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,37 @@
+<script>
+  import _ from './underscore.js'
+  import Moment from './moment.js'
+
+  export default {
+    onLaunch: function() {
+      console.log('App Launch')
+    },
+    onShow: function() {
+      console.log('App Show')
+    },
+    onHide: function() {
+      console.log('App Hide')
+    }
+  }
+</script>
+
+<style lang="scss">
+  /*每个页面公共css */
+  @import "common/common.css";
+  @import "common/iconfont.css";
+  .uni-list-item__extra-text {
+    color: #000000!important;
+    font-size: 28rpx!important;
+  }
+  .uni-list-item__content-title {
+    color: #8F8F94!important;
+  }
+  .select-tab {
+    background-color: #F8F8F8!important;
+    height: 20px!important;
+    margin-top: -10px!important;
+  }
+  .priceTag .is-tag {
+    width: 141px!important;
+  }
+</style>
---
+++ main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import App from './App'
+import uniIcons from "@/components/uni-icons/uni-icons.vue"
+import uniSegmentedControl from "@/components/uni-segmented-control/uni-segmented-control.vue"
+import uniCalendar from '@/components/uni-calendar/uni-calendar.vue'
+import uniCard from '@/components/uni-card/uni-card.vue'
+import uniGoodsNav from '@/components/uni-goods-nav/uni-goods-nav.vue'
+import uniCollapse from '@/components/uni-collapse/uni-collapse.vue'
+import uniCollapseItem from '@/components/uni-collapse-item/uni-collapse-item.vue'
+import uniFav from '@/components/uni-fav/uni-fav.vue'
+import uniDrawer from "@/components/uni-drawer/uni-drawer.vue"
+import uniRate from '@/components/uni-rate/uni-rate.vue'
+import uniTag from "@/components/uni-tag/uni-tag.vue"
+//引入vuex
+import store from './store'
+//把vuex定义成全局组件
+Vue.prototype.$store = store
+
+Vue.config.productionTip = false
+
+export default {
+	components: {
+		uniIcons,
+		uniSegmentedControl,
+		uniCalendar,
+		uniCard,
+		uniGoodsNav,
+		uniCollapse,
+		uniCollapseItem,
+		uniFav,
+		uniDrawer,
+		uniRate,
+    uniTag
+	}
+}
+
+App.mpType = 'app'
+
+const app = new Vue({
+	...App,
+	store
+})
+app.$mount()
